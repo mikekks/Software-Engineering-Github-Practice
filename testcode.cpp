4a2566bbc6cb891af2499a40a71d797f750e5b1c
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-03-27 21:12:38 +0900

    Update testcode.cpp

--- testcode.cpp
+++ testcode.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main(){
-  printf("%s", "Hello Soft-Engineering\n");
+       printf("%s", "Hello Soft-Engineering\n");
   
-  int a = 1;
-  int b = 2;
-  int c = a+b;
+       int a = 1;
+       int b = 2;
+       int c = a+b;
   
-  printf("%d\n", c);
+       printf("%d\n", c);
 
 }
